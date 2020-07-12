7a87d775d9830ea170820733106d8f11ed81ff85
Abhishek Sura <abhisheksura90@gmail.com>
2020-07-13 01:59:13 +0530

    Middle of the Linked List - LeetCode #876

--- Linked Lists/LinkedList.py
+++ Linked Lists/LinkedList.py
@@ -1,9 +1,6 @@
-/*
-    LeetCode #206 - Reverse Linked List
-    https://leetcode.com/problems/reverse-linked-list/
-*/
-
-class LinkedList:    
+class LinkedList:
+    # LeetCode #206 - Reverse Linked List
+    # https://leetcode.com/problems/reverse-linked-list/
     def reverseList(self, head: ListNode) -> ListNode:
         prev = None
         current = head
@@ -17,3 +14,16 @@ class LinkedList:
             prev = current
             current = next
         return prev
+
+    # LeetCode #876 - Middle of the Linked List
+    # https://leetcode.com/problems/middle-of-the-linked-list/
+    # TC - O(N) No of nodes
+    def middleNode(self, head: ListNode) -> ListNode:
+        if head is None:
+            return None
+
+        fast, slow = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
